4d7562ec08b908ad8732e63635030a19d9b17400
zytronium <dastelljes@outlook.com>
2025-06-29 16:20:23 -0500

    Added script to redirect to main domain from GitHub Pages CNAME would redirect all GH Pages projects, which I don't want

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Happy developing ✨')
+// Redirect from GitHub Pages root, but not from other GitHub Pages projects
+if (location.hostname === 'zytronium.github.io' &&
+    (location.pathname === '/' || location.pathname === '')) {
+    location.replace('https://zytronium.dev/');
+}
